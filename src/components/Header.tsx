0464d305c1ca500f8f44728ca68b0d903d4d7a9f
oman ramalho <omanapple42@hotmail.com>
2024-01-08 14:04:53 -0300

    upping size logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header:React.FC = () => {
   return (
     <div className='flex flex-row 2xl:mx-20 mx-10 2xl:h-[75px] h-full items-center justify-between p-[10px]'>
-      <h5 className='uppercase sm:text-[64px] text-[34px] tracking-[6px]'>
+      <h5 className='uppercase sm:text-[64px] text-[44px] tracking-[6px]'>
         KONDA
       </h5>
       <nav className='flex items-center 2xl:space-x-10 space-x-5'>
