c7c9730a74d6bbb77449043a89a0dbe67b2fe9d9
oman ramalho <omanapple42@hotmail.com>
2024-01-16 20:21:13 -0300

    make effect reveal

--- src/screens/Inspiration.tsx
+++ src/screens/Inspiration.tsx
@@ -7,6 +7,7 @@ import Image2 from '../assets/img/Travel Map - LED Neon Sign, Wall Decor, Wall S
 import Image3 from '../assets/img/Cuadro de CR7 - 42cm x 63cm 1.png'
 
 import { ArrowLeft, ArrowRight } from '@phosphor-icons/react'
+import Reveal from '@/components/Reveal'
 
 const Inspiration = () => {
     const neonImages = [
@@ -17,35 +18,39 @@ const Inspiration = () => {
 
   return (
     <div className='flex flex-col xl:h-[100vh] h-full xl:mx-20 mx-5 xl-mt-0'>
-        <div className='flex w-full flex-row justify-between items-center'>
-            <div 
-                className='flex justify-start items-start w-[380px] border-b-2' 
-                style={{ 
-                    borderStyle: 'solid',
-                    borderImage: 'linear-gradient(to right, #63ABFD, #F6F7A0, #FF5E82) 1'
-                }}
-            >
-                <p className='uppercase sm:text-[64px] text-[44px] tracking-[6px] text-bold'>
-                    Inspiraçao
-                </p>
-            </div>
+        <Reveal>
+            <div className='flex w-full flex-row justify-between items-center'>
+                <div 
+                    className='flex justify-start items-start w-[380px] border-b-2' 
+                    style={{ 
+                        borderStyle: 'solid',
+                        borderImage: 'linear-gradient(to right, #63ABFD, #F6F7A0, #FF5E82) 1'
+                    }}
+                >
+                    <p className='uppercase sm:text-[64px] text-[44px] tracking-[6px] text-bold'>
+                        Inspiraçao
+                    </p>
+                </div>
 
-            <div className='flex justify-center items-center'>
-                <div className='flex border-1 rounded-full p-3'>
-                    <ArrowLeft size={32} color='#fff'/>
-                </div>
-                <div className='border-1 rounded-full p-3'>
-                    <ArrowRight size={32} color='#fff'/>
+                <div className='flex justify-center items-center'>
+                    <div className='flex border-1 rounded-full p-3'>
+                        <ArrowLeft size={32} color='#fff'/>
+                    </div>
+                    <div className='border-1 rounded-full p-3'>
+                        <ArrowRight size={32} color='#fff'/>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Reveal>
 
         <div className='grid xl:grid-cols-3 grid-cols-1 gap-10 w-full justify-center items-center xl:h-[100px] h-full my-[28px]'>
             { neonImages.map((item, index) => {
                 return (
-                    <div key={index} className='flex w-full h-[565px] '>
-                        <Image src={item.image} alt={'foto dw neon'} className='w-full object-cover rounded-xl h-full' />
-                    </div>
+                    <Reveal key={index} mult={index}>
+                        <div className='flex w-full h-[565px] '>
+                            <Image src={item.image} alt={'foto dw neon'} className='w-full object-cover rounded-xl h-full' />
+                        </div>
+                    </Reveal>
                 )
             })}
         </div>
